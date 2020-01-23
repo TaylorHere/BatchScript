e5aa9d7954edc75664b721fb7a074aa7da0f19aa
Here Taylor <taylorherelee@gmail.com>
2020-01-23 21:02:29 +0800

    feat: 修改jobs put

--- main.py
+++ main.py
@@ -6,17 +6,4 @@ from consumer import Consumer
 master = Master(func=helper.sleep, worker=Worker, result_callback=print)
 master.start()
 
-queue_id = 0
-
-def get_queue():
-    queue = master.jobQueues[queue_id]
-    if queue_id + 1 > len(master.jobQueues):
-        queue_id = 0
-    else:
-        queue_id += 1
-
-def put_in_queue(msg):
-    queue = get_queue()
-    queue.put(msg)
-
-Consumer(callback=put_in_queue).start()
+Consumer(callback=master.jobs().put).start()

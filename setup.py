472dd7e5b03be821eabea588fcc25a8d976d4260
Here Taylor <taylorherelee@gmail.com>
2020-01-27 00:38:08 +0800

    feat:

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="BatchScript",
-    version="0.0.4",
+    version="0.0.5",
     author="TaylorHere",
     author_email="taylorherelee@gmail.com",
     description="Python Master-Queue-Worker Structure multiple task framework",

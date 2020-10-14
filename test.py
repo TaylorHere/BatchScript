7862102aa81c4281f1bb277200828b4a0bdc07a4
Taylorli <taylorli@lilith.com>
2020-10-14 17:37:28 +0800

    feat:

--- test.py
+++ test.py
@@ -3,23 +3,32 @@ from BatchScript.worker import Worker
 import BatchScript.helper
 import BatchScript.config
 import time
+
+#this is my function need to run in batch mode
 def timer(id):
     for i in range(10 ** 6):
         _ = 1 + 1
     return id, time.time()
 
-master = Master(func=timer, worker=Worker, result_callback=print)
+BatchScript.config.WorkerGetBatchSize = BatchScript.config.MaxThreadPoolSize * 10
+
+#start a master
+master = Master(func=timer, worker=Worker, result_callback=print, config=BatchScript.config)
 master.start()
 
-for i in range(5000):
+
+Job_Conut = BatchScript.config.MaxWorkerSize * 1024
+
+#create a lot of datas
+for i in range(Job_Conut):
     master.jobs().put(i)
 
-from queue import Empty
 
 results = []
+#wait all datas done
 while True:
     results.append(master.results().get())  #this will return one results queue by round robin
-    if len(results) == 5000:
+    if len(results) == Job_Conut:
         break
 
 print("\033[32m Run with BatchScript \033[0m")
@@ -29,10 +38,11 @@ _, start = results[0]
 _, end = results[-1]
 print(end-start)
 
+#stop the master and the workers will terminated as well
 master.stop()
 
 results = []
-for i in range(5000):
+for i in range(Job_Conut):
     results.append(timer(i))
 
 print("\033[31m Run in a loop \033[0m")
